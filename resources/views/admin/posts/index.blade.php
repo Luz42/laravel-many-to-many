b87d20acc2d46abbda1ab9edff0aeae9cd868bfd
Luz42 <a.evangelista93@gmail.com>
2022-11-15 23:21:01 +0100

    implementata la visualizzazione base dei post in index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    @dump($posts)
+    @foreach ($posts as $post)
+        <a href="{{ route('admin.posts.show', $post->id) }}">{{$post->title}}</a>
+    @endforeach
 @endsection
