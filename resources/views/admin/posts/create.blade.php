7ffb7d7fed2ff121d09c4430b982a70c3607a004
Luz42 <a.evangelista93@gmail.com>
2022-11-16 12:08:01 +0100

    implementata la visualizzazione degli errori lato backend

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -4,13 +4,19 @@
     
     <form action="{{route('admin.posts.store')}}" method="post">
         @csrf
-        <div>
+        <div @error('title') class="is-invalid" @enderror>
             <label for="title">{{__('Titolo')}}</label>
             <input name="title" type="text" required value="{{old('title','')}}">
+            @error('title')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
-        <div>
+        <div @error('content') class="is-invalid" @enderror>
             <label for="content">{{__('Contenuto')}}</label>
             <textarea name="content" cols="20" rows="5" required>{{old('content','')}}</textarea>
+            @error('content')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <input type="submit" value="{{__('Crea')}}">
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -8,10 +8,16 @@
     <div>
         <label for="title">{{__('Titolo')}}</label>
         <input name="title" type="text" required value="{{old('title', $post->title)}}">
+        @error('title')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
     <div>
         <label for="content">{{__('Contenuto')}}</label>
         <textarea name="content" cols="20" rows="5" required>{{old('content',$post->content)}}</textarea>
+        @error('content')
+                <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
     <div>
         <input type="submit" value="{{__('Aggiorna')}}">
